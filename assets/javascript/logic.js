a2018f3bc551276e0a4d31e44d646f232dbd3d6c
rebecca8840 <31925939+rebecca8840@users.noreply.github.com>
2018-01-28 13:00:14 -0500

    Add files via upload

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -2,7 +2,7 @@ $( document ).ready(function() {
     console.log( "ready!" );
 
     $("button").on("click", function() {
-    	var city = (this).attr("data-city");
+    	var city = $(this).attr("data-city");
 
     	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
         city + "&api_key=aRzEvvatJe5JykmA2OazGtZWxh3SW1Gc" 
@@ -10,38 +10,41 @@ $( document ).ready(function() {
         $.ajax({
         	 
         	 url: queryURL,
-        	 method: 'GET'     
+        	 method: 'GET'  
 
-        	   }).then(function(response) {
-        	   		console.log(response);
+        	   })
+
+        .then(function(response) {
+
+        	   		var results = response.data;
+
+
+        	   		 	for (var i = 0; i < results.length; i++) {
+
+        	   		 		var gifDiv = $("<div class = 'item'>");
+
+
+	        	   		 	var rating = results[i].rating
+
+	        	   		 	var p = $("<p>").text("Rating: " + rating);
+
+
+	        	   		 	var cityImage = ("<img>");
+
+	        	   		 	cityImage.attr("src", results[i].images.fixed_height.url);
+
+
+	        	   		 	gifDiv.prepend(p);
+
+	        	   		 	gifDiv.prepend(cityImage);
+
+
+	        	   		 	$("gifsHere").prepend(gifDiv);
+
+
+	        	   		 		console.log(response);
+
+        	   		 	}
+        	   		});
         	   });
-
-
-
     });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//end of document parenthesis below    
-});
